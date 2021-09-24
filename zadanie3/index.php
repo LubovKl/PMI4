56fc5378ea3e1f84d39e9b40790bdbf56831d99b
LubovKl <18000045@edu.spbgasu.ru>
2021-09-24 10:56:50 +0000

    1

---
+++ zadanie3/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>
+   Блочный тип
+</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <div class="relative">
+            Блок 1
+        </div>
+        <div class="absolute">
+            Блок 2
+        </div>
+        <div class="fixed">
+            Блок 3
+        </div>
+</body>
+</html>
