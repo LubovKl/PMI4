33ec059723689dc3005b89ba8d9d8aa8708b5bf3
LubovKl <18000045@edu.spbgasu.ru>
2021-09-30 13:37:25 +0000

    A

--- zadanie3/index.php
+++ zadanie3/index.php
@@ -9,14 +9,52 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
-   <div class="relative">
-            Блок 1
-        </div>
-        <div class="absolute">
-            Блок 2
+   <div class="static">
+            static
         </div>
         <div class="fixed">
-            Блок 3
+            fixed
         </div>
+        <div class="float1">
+            float
+        </div>
+        <div class="float2">
+            
+        </div>
+    
+    
+        <div class="relative">
+            relative
+        <div class="absolute1">
+            absolute
+        </div>
+        <div class="absolute2">
+            absolute
+        </div>
+        <div class="absolute3">
+            absolute
+        </div>
+        <div class="absolute4">
+            absolute
+        </div>
+        </div>  
+            
+            
+        <div class="float3">
+            Float
+        </div>
+        <div class="float4">
+            Float
+        </div>
+        <div class="float5">
+            Float
+        </div>
+        <div class="float6">
+            Float
+        </div>
+        <div class="float7">
+            Float
+        </div>
+         
 </body>
 </html>
---
+++ zadanie4/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<html>
+    <head>
+        <title>Display flext</title>
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" href="style.css?<?php echo rand();?>">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <style>
+            
+        </style>
+    </head>
+    <body>
+        
+        <div class="main_block">
+            <div class="item">0</div>
+            <div class="item">1</div>
+            <div class="item">2</div>
+            <div class="item">3</div>
+            <div class="item">4</div>
+            <div class="item">5</div>
+            <div class="item">6</div>
+            <div class="item">7</div>
+            <div class="item">8</div>
+            <div class="item">9</div>
+        </div>
+    </body>
+</html>
