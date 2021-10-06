0cd2ac02f8165f0e22fca5b59a7f391070ae146d
LubovKl <18000045@edu.spbgasu.ru>
2021-10-06 08:00:30 +0000

    this is my third  commit

---
+++ s1/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>
+   Блочный тип
+</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <div class="staticBox">
+            static
+        </div>
+        <div class="fixedBox">
+            fixed
+        </div>
+        <div class="floatBox">
+            float
+        </div>
+        <div class="floatBox"></div>
+    
+        <div class="relativeBoxes" id="relativeBox">
+            relative
+            <div class="absoluteBoxes" id="absBoxLT">
+            absolute
+        </div>
+            <div class="absoluteBoxes" id="absBoxLB">
+            absolute
+        </div>
+            <div class="absoluteBoxes" id="absBoxRT">
+            absolute
+        </div>
+            <div class="absoluteBoxes" id="absBoxRB">
+            absolute
+        </div>
+        </div>  
+    
+        <div class="floatBoxB">
+            Float
+        </div>
+        <div class="floatBoxB">
+            Float
+        </div>
+        <div class="floatBoxB">
+            Float
+        </div>
+        <div class="floatBoxB">
+            Float
+        </div>
+        <div class="floatBoxB">
+            Float
+        </div>
+</body>
+</html>
