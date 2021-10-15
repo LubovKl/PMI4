cddb8a10a23f025706e83f150fa5593b40c7659d
LubovKl <18000045@edu.spbgasu.ru>
2021-10-15 09:51:44 +0000

    this is my 6 commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+//let user = "Ivam",
+//age = 25,
+//message = 'Hello';
+//
+//user = {"name":"Ivan"};
+//let my_alert = function( str ){ alert(str); };
+
+//let bool = 0;
+//if ( bool > -1 ){
+//    alert(23)
+//    alert(24)
+//}else if ( bool > 2 ){
+//    
+//}else{
+//    
+//}
+
+//let rat = ( "A" > "b" ) ? "b" : "A";
+
+/*
+alert(3 +
+1        
++ 2);
+ */
+
+//alert(7);
+//[1,4].forEach(alert);         (ctrl+/)
+
