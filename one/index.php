de05a0d0ee11951018ef928f3f50cfd620fb516e
LubovKl <18000045@edu.spbgasu.ru>
2021-11-13 17:44:04 +0000

    this is my 9 commit

---
+++ one/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Первый сайт</title>
+        
+        <link rel="stylesheet" href="/style.css">
+    </head>
+    
+    <body>
+        <h1>
+            Первый сайт!
+        </h1>
+        <h2>
+            Первый сайт!
+        </h2>
+        <h3>
+            Первый сайт!
+        </h3>
+        <p>
+            Hello
+        </p>
+        <span id="Petr" class="Ivanov" style="display: block; text-align: center; color: blue">
+            Petr
+        </span>
+        <a>
+            123445
+        </a>
+        
+        <div>I am DIV</div>
+        
+        <ul>
+            <li>2</li>
+            <li>3</li>
+            <li>1</li>
+        </ul>
+        
+        <ol>
+            <li>2</li>
+            <li>3</li>
+            <li>1</li>
+            <li>Content
+                <ul>
+                    <li>
+                        232
+                    </li>
+                </ul>
+            </li>
+        </ol>
+        
+        <div class="absolute">
+            Absolute
+        </div>
+        <div id="fixed">
+            Fixed
+        </div>
+        <div class="div relative">
+            Relative 
+            <span class="absolute">Content</span>
+            <div id="fixed">Content</div>
+        </div>
+        <div>
+            
+        </div>
+    </body>
+</html>
