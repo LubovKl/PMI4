151e0d9cb545b8268f039ec086485bc3b53df323
LubovKl <90001850+LubovKl@users.noreply.github.com>
2022-02-11 16:40:48 +0300

    Add files via upload

---
+++ mvc/controllers/catalog.php
@@ -0,0 +1,12 @@
+<?php
+
+
+class Catalog{
+public function __construct(){
+echo 'Catalog<br/>';
+}
+
+public function show_list(){
+    echo'method show list<br/>';
+}
+}
---
+++ mvc/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     <meta charset="UTF-8">
+     <title>
+         MVC
+     </title>
+    </head>
+    <body>
+        <div class="back">
+    <a href="http://134.122.94.44/">Назад</a>
+        </div>
+        
+        <script>
+
+        </script>
+    </body>
+</html>
+
+
+
+
