1975da86ed3f08fac72e23833f1996be4f6fb18d
LubovKl <90001850+LubovKl@users.noreply.github.com>
2022-02-14 17:55:27 +0300

    Add files via upload

---
+++ mvc/libs/View.php
@@ -0,0 +1,10 @@
+<?php
+
+class View {
+    public function __construct() {
+    }
+    
+    public function render($path ) {
+        require $_SERVER['DOCUMENT_ROOT'].'/mvc/views/'.$path.'/index.php';
+    }
+}
---
+++ mvc/views/catalog/show_list/index.php
@@ -0,0 +1,11 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/mvc/views/header.php'?>
+
+<?php
+
+foreach($this->data ['LIST'] as $section ){
+    echo $section.'<br/>';
+}
+
+?>
+
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/mvc/views/footer.php'?>
