ff1f426ec1c1ad789ab92069a61d540e6a00a2e4
LubovKl <90001850+LubovKl@users.noreply.github.com>
2022-02-18 10:30:43 +0300

    Add files via upload

---
+++ mvc/views/catalog/index.php
@@ -0,0 +1,9 @@
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/mvc/views/header.php'?>
+
+<?php
+
+echo 'View Catalog <br/>';
+
+?>
+
+<?php require_once $_SERVER['DOCUMENT_ROOT'].'/mvc/views/footer.php'?>
