8c16ddd1c0d3263287862853cf775c50491738a3
LubovKl <90001850+LubovKl@users.noreply.github.com>
2022-02-18 10:30:16 +0300

    Delete mvc directory

--- mvc/views/index/index.php
+++
@@ -1,9 +0,0 @@
-<?php require_once $_SERVER['DOCUMENT_ROOT'].'/mvc/views/header.php'?>
-
-<?php
-
-echo 'View Index <br/>';
-
-?>
-
-<?php require_once $_SERVER['DOCUMENT_ROOT'].'/mvc/views/footer.php'?>
