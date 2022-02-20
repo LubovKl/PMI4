82c188eeea81e66dec4fc87e07365f0bf2f1ba7e
LubovKl <90001850+LubovKl@users.noreply.github.com>
2022-02-20 22:10:57 +0300

    Add files via upload

---
+++ mvc/index1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+     <meta charset="UTF-8">
+     <title>
+         MVC
+     </title>
+    </head>
+    <body>
+        <a href="/mvc">
+            Реализация MVC
+        </a>
+    </body>
+</html>
+
+
+
+
---
+++ mvc/libs/Config.php
@@ -0,0 +1,6 @@
+<?php
+
+define("DBNAME",'ck27434_mvc');
+define("DBUSER",'ck27434_mvc');
+define("DBPASS",'mvcmvc');
+define('BASE_DIR','/mvc');
